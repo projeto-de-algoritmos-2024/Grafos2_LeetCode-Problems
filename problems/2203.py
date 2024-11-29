e5ffce4c9f57ca197aa397bf22b850ff062303b2
Izabella Alves <iap21010705@gmail.com>
2024-11-29 17:41:44 -0300

    feat: adiciona implementacao de dijkstra

--- problems/2203.py
+++ problems/2203.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import defaultdict
+import heapq
 
 class Solution:
     def minimumWeight(self, n: int, edges: List[List[int]], src1: int, src2: int, dest: int) -> int:
@@ -10,4 +11,22 @@ class Solution:
         for origem, destino, peso in edges:
             grafoNormal[origem].append((destino, peso))
             grafoReverso[destino].append((origem, peso))
+            
+        infinito = float('inf')
+        
+        def dijkstra(grafo, pontoPartida):
+            distancias = [infinito] * n 
+            distancias[pontoPartida] = 0 
+            filaPrioridade = [(0, pontoPartida)] 
+
+            while filaPrioridade: 
+                distanciaAtual, no = heapq.heappop(filaPrioridade) 
+                if distanciaAtual > distancias[no]: 
+                    continue
+                for vizinho, peso in grafo[no]: 
+                    novaDistancia = distancias[no] + peso 
+                    if distancias[vizinho] > novaDistancia: 
+                        distancias[vizinho] = novaDistancia
+                        heapq.heappush(filaPrioridade, (novaDistancia, vizinho))
+            return distancias
         
