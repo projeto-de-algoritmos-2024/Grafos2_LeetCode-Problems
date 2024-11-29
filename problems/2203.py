0621c3be8f4d25dadde8e321b8495251e3fd1f0f
Izabella Alves <iap21010705@gmail.com>
2024-11-29 17:27:10 -0300

    feat: cria o grafo normal e o grafo reverso

--- problems/2203.py
+++ problems/2203.py
@@ -1,2 +1,13 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def minimumWeight(self, n: int, edges: List[List[int]], src1: int, src2: int, dest: int) -> int:
+        
+        grafoNormal = defaultdict(list)
+        grafoReverso = defaultdict(list)
+
+        for origem, destino, peso in edges:
+            grafoNormal[origem].append((destino, peso))
+            grafoReverso[destino].append((origem, peso))
+        
