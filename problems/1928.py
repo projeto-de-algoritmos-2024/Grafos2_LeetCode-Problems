db3351a6c8424cf08e4d442553cccdcf92baf7bc
Gabriel Zaranza <gabrielzaranza@hotmail.com>
2024-12-01 10:50:57 -0300

    problema 1928: trocando a ordem da tupla inserida na heap, para a heap considerar o custo como prioridade o custo da viagem

--- problems/1928.py
+++ problems/1928.py
@@ -5,61 +5,60 @@
 # In the beginning, you are at city 0 and want to reach city n - 1 in maxTime minutes or less. The cost of your journey is the summation of passing fees for each city that you passed through at some moment of your journey (including the source and destination cities).
 
 # Given maxTime, edges, and passingFees, return the minimum cost to complete your journey, or -1 if you cannot complete it within maxTime minutes.
-
-def minCost(maxTime: int, edges: list[list[int]], passingFees: list[int]) -> int:
-    import heapq
-    import pprint
-    
-    def cria_grafo(n, edges):
-        grafo = {}
-        for i in range(n):
-            grafo[i] = []
-            
-        for i in range(n):
-            u = edges[i][0]
-            v = edges[i][1]
-            peso = edges[i][2]
-            grafo[u].append((v, peso))
-            grafo[v].append((u, peso))
-              
-        pprint.pprint(grafo)
-        return grafo
-    
-    n = len(passingFees)
-    
-    grafo = cria_grafo(n, edges)
-    
-    tempos = [int('inf')] * n
-    
-    heap = [(0, 0, passingFees[0])] #(tempo, no, custo)
-    
-    while heap:
+class Solution:
+    def minCost(self, maxTime: int, edges: list[list[int]], passingFees: list[int]) -> int:
+        import heapq
+        import pprint
         
-        tempo_atual, no_atual, custo_atual = heapq.heappop(heap)
-        
-        print(f"tempo atual = {tempo_atual} no atual = {no_atual} custo atual = {custo_atual}")
-        
-        if tempo_atual > maxTime:
-            print('primeiro return')
-            return -1
-
-        if no_atual == n-1:
-            print('segundo return')
-            return custo_atual
-
-        for vizinho, tempo in grafo[no_atual]:
-            print('aaa')
-            tempo_vizinho = tempo_atual + tempo
-            custo_vizinho = custo_atual + passingFees[vizinho]
-            
-            if tempo_vizinho < tempos[vizinho]:
+        def cria_grafo(n, edges):
+            grafo = {}
+            for i in range(n):
+                grafo[i] = []
                 
-                tempos[vizinho] = tempo_vizinho
-                heapq.heappush(heap,(tempo_vizinho,vizinho,custo_vizinho))
-    
-     
-maxTime = 30
-edges = [[0,1,10],[1,2,10],[2,5,10],[0,3,1],[3,4,10],[4,5,15]]
-passingFees = [5,1,2,20,20,3]
+            for i in range(n):
+                u = edges[i][0]
+                v = edges[i][1]
+                peso = edges[i][2]
+                grafo[u].append((v, peso))
+                grafo[v].append((u, peso))
+                
+            
+            return grafo
+        
+        n = len(passingFees)
+        
+        grafo = cria_grafo(n, edges)
+        
+        tempos = [1001] * n
+        tempos[0] = 0
+        heap = [(passingFees[0], 0, 0)] #(custo, no, tempo)
+        
+        while heap:
+            
+            custo_atual, no_atual, tempo_atual = heapq.heappop(heap)
+            
+            
+            if tempo_atual > maxTime:
+                
+                return continue
 
-minCost(maxTime=maxTime,edges=edges,passingFees=passingFees)
+            if no_atual == n-1:
+                
+                return custo_atual
+
+            for vizinho, tempo in grafo[no_atual]:
+                if tempos[vizinho] == 1001:
+                    tempo_vizinho = tempo_atual + tempo
+                    custo_vizinho = custo_atual + passingFees[vizinho]
+                    
+                    if tempo_vizinho < tempos[vizinho]:
+                        
+                        tempos[vizinho] = tempo_vizinho
+                        heapq.heappush(heap,(custo_vizinho,vizinho,tempo_vizinho))
+            
+     
+#maxTime = 30
+#edges = [[0,1,10],[1,2,10],[2,5,10],[0,3,1],[3,4,10],[4,5,15]]
+#passingFees = [5,1,2,20,20,3]
+
+#print(Solution.minCost(maxTime=maxTime,edges=edges,passingFees=passingFees))
