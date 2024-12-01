cd530a376612bb1b493c00f71a498a87085547a5
Gabriel Zaranza <gabrielzaranza@hotmail.com>
2024-12-01 10:24:59 -0300

    problema 1928: criando a funcao cria_grafo e testando a heap

--- problems/1928.py
+++ problems/1928.py
@@ -7,93 +7,55 @@
 # Given maxTime, edges, and passingFees, return the minimum cost to complete your journey, or -1 if you cannot complete it within maxTime minutes.
 
 def minCost(maxTime: int, edges: list[list[int]], passingFees: list[int]) -> int:
+    import heapq
     import pprint
-    num_nodes = len(passingFees)
+    
+    def cria_grafo(n, edges):
+        grafo = {}
+        for i in range(n):
+            grafo[i] = []
+            
+        for i in range(n):
+            u = edges[i][0]
+            v = edges[i][1]
+            peso = edges[i][2]
+            grafo[u].append((v, peso))
+            grafo[v].append((u, peso))
+              
+        pprint.pprint(grafo)
+        return grafo
+    
+    n = len(passingFees)
+    
+    grafo = cria_grafo(n, edges)
+    
+    tempos = [0] * n
+    
+    heap = [(0, 0, passingFees[0])] #(tempo, no, custo)
+    
+    while heap:
         
-    grafo = []
-    for i in range(num_nodes):
-        grafo.append([])
-        
-    for e in edges:
-        grafo[e[0]].append((e[1], e[2]))
+        tempo_atual, no_atual, custo_atual = heapq.heappop(heap)
         
         
-    pprint.pprint(grafo)
+        
+        if tempo_atual > maxTime:
+            return -1
 
+        if no_atual == n-1:
+            return custo_atual
+
+        for vizinho, tempo in grafo[no_atual]:
+            tempo_vizinho = tempo_atual + tempo
+            custo_vizinho = custo_atual + passingFees[vizinho]
+            
+            if tempo_atual < tempos[vizinho]:
+                tempos[vizinho] = tempo_atual
+                heapq.heappush(heap,(tempo_atual,vizinho,custo_vizinho))
+    
+     
 maxTime = 30
 edges = [[0,1,10],[1,2,10],[2,5,10],[0,3,1],[3,4,10],[4,5,15]]
 passingFees = [5,1,2,20,20,3]
 
-minCost(maxTime=maxTime,edges=edges,passingFees=passingFees)
-
-# import pprint
-#     class MinHeap:
-#         def __init__(self):
-#             self.heap = []
-
-#         def push(self, val):
-#             # Adiciona o elemento no final
-#             self.heap.append(val)
-#             # Reorganiza a heap para manter a propriedade mínima
-#             self._heapify_up(len(self.heap) - 1)
-
-#         def pop(self):
-#             if not self.heap:
-#                 raise IndexError("Pop from empty heap")
-#             # Troca o primeiro e último elemento
-#             self._swap(0, len(self.heap) - 1)
-#             # Remove o menor elemento (raiz)
-#             min_val = self.heap.pop()
-#             # Reorganiza a heap
-#             self._heapify_down(0)
-#             return min_val
-
-#         def peek(self):
-#             if not self.heap:
-#                 raise IndexError("Peek from empty heap")
-#             return self.heap[0]
-
-#         def _heapify_up(self, index):
-#             parent = (index - 1) // 2
-#             # Enquanto não estiver na raiz e o elemento for menor que o pai
-#             if index > 0 and self.heap[index] < self.heap[parent]:
-#                 self._swap(index, parent)
-#                 self._heapify_up(parent)
-
-#         def _heapify_down(self, index):
-#             smallest = index
-#             left = 2 * index + 1
-#             right = 2 * index + 2
-#             # Verifica o filho esquerdo
-#             if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
-#                 smallest = left
-#             # Verifica o filho direito
-#             if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
-#                 smallest = right
-#             # Se o menor não for o nó atual, troca e continua descendo
-#             if smallest != index:
-#                 self._swap(index, smallest)
-#                 self._heapify_down(smallest)
-
-#         def _swap(self, i, j):
-#             self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
-#     def constroi_grafo(num_nodes: int, edges: list[list[int]]) -> dict:
-    
-#         grafo = {}
-        
-#         for i in range(num_nodes):
-#             grafo[i]= {'vizinhos':[],'pesos':[],'pedagio':-1}
-#         return grafo
-        
-#     grafo = constroi_grafo(len(passingFees),edges)
-    
-#     heap = MinHeap()
-    
-#     for i in edges:
-#         grafo[i[0]]['vizinhos'].append(i[1])
-#         grafo[i[0]]['pesos'].append(i[2])
-#         grafo[i[0]]['pedagio']=passingFees[i[0]]
-#     grafo[len(passingFees)-1]['pedagio']=passingFees[-1]   
-    
-        
-#     pprint.pprint(grafo)
+print(minCost(maxTime=maxTime,edges=edges,passingFees=passingFees))
